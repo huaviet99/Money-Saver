2b8256eb9cfdfce471af5660777fe31b7a2a97ed
Viet Hua <huaviet999@gmail.com>
2022-01-19 15:54:22 +0700

    init commit

---
+++ Money Saver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money Saver
+//
+//  Created by Viet Hua on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
